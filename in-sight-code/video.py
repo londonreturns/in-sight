a892668a8ba53285e9dc0661831caff4de22e465
londonreturns <subedioscar.47@gmail.com>
2025-04-14 14:01:22 +0545

    user id added to video in db

--- in-sight-code/video.py
+++ in-sight-code/video.py
@@ -2,16 +2,23 @@ from bson import ObjectId
 from flask import make_response
 import gridfs
 from database import open_connection, close_connection
+from user import checkIfUserExists
 
 
-def store_video(video):
+def store_video(video, email=None):
+    user = checkIfUserExists(email)
+
+    if user is None:
+        return {"error": "User not found"}, 404
+
     db, client = open_connection()
     fs = gridfs.GridFS(db)
     video_id = fs.put(video, filename=video.filename)
     video_document = {
         "_id": video_id,
         "filename": video.filename,
-        "content_type": video.content_type
+        "content_type": video.content_type,
+        "user_id": user['_id']
     }
     db.videos.insert_one(video_document)
     close_connection(client)
