6821355f1777714ce3fc6edbf5aea7adb4898d0d
londonreturns <subedioscar.47@gmail.com>
2025-04-14 12:50:49 +0545

    video file created

--- in-sight-code/database.py
+++ in-sight-code/database.py
@@ -1,8 +1,5 @@
-from bson import ObjectId
-from flask import make_response
 from pymongo import MongoClient
 from os import getenv
-import gridfs
 
 
 def open_connection():
@@ -13,28 +10,3 @@ def open_connection():
 
 def close_connection(client):
     client.close()
-
-
-def store_video(video):
-    db, client = open_connection()
-    fs = gridfs.GridFS(db)
-    video_id = fs.put(video, filename=video.filename)
-    video_document = {
-        "_id": video_id,
-        "filename": video.filename,
-        "content_type": video.content_type
-    }
-    db.videos.insert_one(video_document)
-    close_connection(client)
-    return video_id
-
-
-def query_video(video_id):
-    db, client = open_connection()
-    fs = gridfs.GridFS(db)
-    video_file = fs.get(ObjectId(video_id))
-    response = make_response(video_file.read())
-    response.headers['Content-Type'] = video_file.content_type
-    response.headers['Content-Disposition'] = f'attachment; filename={video_file.filename}'
-    close_connection(client)
-    return response
---
+++ in-sight-code/video.py
@@ -0,0 +1,29 @@
+from bson import ObjectId
+from flask import make_response
+import gridfs
+from database import open_connection, close_connection
+
+
+def store_video(video):
+    db, client = open_connection()
+    fs = gridfs.GridFS(db)
+    video_id = fs.put(video, filename=video.filename)
+    video_document = {
+        "_id": video_id,
+        "filename": video.filename,
+        "content_type": video.content_type
+    }
+    db.videos.insert_one(video_document)
+    close_connection(client)
+    return video_id
+
+
+def query_video(video_id):
+    db, client = open_connection()
+    fs = gridfs.GridFS(db)
+    video_file = fs.get(ObjectId(video_id))
+    response = make_response(video_file.read())
+    response.headers['Content-Type'] = video_file.content_type
+    response.headers['Content-Disposition'] = f'attachment; filename={video_file.filename}'
+    close_connection(client)
+    return response
