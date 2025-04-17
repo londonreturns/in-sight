105435ff86857a856a134a08346ef37ed3739d53
londonreturns <subedioscar.47@gmail.com>
2025-04-17 20:27:38 +0545

    session lifetime changed

--- in-sight-code/app.py
+++ in-sight-code/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = getenv('MY_SECRET_KEY')
 app.config['MONGO_URI'] = getenv('DATABASE_CONNECTION_STRING')
-app.permanent_session_lifetime = timedelta(minutes=10)
+app.permanent_session_lifetime = timedelta(minutes=60)
 app.register_blueprint(routes, url_prefix='')
 
 if __name__ == '__main__':
